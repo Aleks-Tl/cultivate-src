57744366d2b67086f776b4f22588d063055eec9f
Aleksandr Tloka <aleksandr.tloka@i.ua>
2022-05-31 14:26:25 +0300

    doing slider section home page

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,33 @@ function scrollFunction() {
     document.querySelector('header').classList.remove('backgroundWhite');
   }
 }
+
+// Swiper for home page
+
+const swiper = new Swiper(".members__slider", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 768px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
+});
